6b965b777102268907665d82799c8a612df6b14e
Ben Melikant <bmmelikant@gmail.com>
2021-03-14 21:21:18 -0400

    [master] saving initial work

---
+++ graphics-demo.asm
@@ -0,0 +1,166 @@
+org 0x0100
+bits 16
+
+%define TILE_PIXEL_WIDTH    8
+%define TILE_PIXEL_HEIGHT   8
+
+%define X_TILES (320/TILE_PIXEL_WIDTH)
+%define Y_TILES (200/TILE_PIXEL_HEIGHT)
+
+%define VMEM_SEGMENT       0xA000
+%define PIXELS_PER_ROW     320
+
+    jmp start
+
+tile_data:
+
+db 02h,02h,02h,02h,02h,02h,02h,02h
+db 02h,06h,02h,06h,06h,06h,02h,06h
+db 02h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
+
+start:
+
+    push cs
+    pop ds
+    pop es
+
+    mov ax,0x0013
+    int 0x10
+
+    call demo
+
+    mov ah,0x01
+    int 0x21
+
+    mov ax,0x0003
+    int 0x10
+
+    mov ax,0x4c00
+    int 0x21
+
+demo:
+
+    mov cx,
+
+    push word 0
+    push word 0
+    push word tile_data
+
+    call draw_tile
+    add sp,6
+
+    push word 0
+    push word 8
+    push word tile_data
+    call draw_tile
+    add sp,6
+
+    ret
+
+; draw_rectangle - draw a rectangle on the screen
+; push arguments in reverse order:
+; draw_rectangle(word start_x, word start_y, word end_x, word end_y, word color_attr)
+draw_rectangle:
+
+    push bp
+    mov bp,sp
+
+    push VMEM_SEGMENT
+    pop es
+
+    mov cx,word [bp+8]      ; start_y goes into cx
+
+.row:
+
+    push cx
+
+    mov ax,PIXELS_PER_ROW
+    mul cx                      ; (y_pos * PIXELS_PER_ROW)
+    xchg ax,cx                  ; put the result back into cx
+
+    ; put the starting location in bx
+    mov bx,word [bp+10]                  ; start_x goes into bx
+    add bx,cx                           ; (y_pos * PIXELS_PER_ROW) + x_pos
+
+    mov cx,word [bp+6]                  ; end x position
+    sub cx,word [bp+8]                  ; subtract the start x position
+    mov dx,word [bp+10]                ; color attribute
+
+.column:
+
+    mov [es:bx],dl          ; store the color in es:bx
+    inc bx                  ; increment bx
+    loop .column          ; and loop cx times
+
+    ; go to the next row
+    pop cx
+    inc cx
+    cmp cx,word [bp+4]      ; check to see if we hit end_y yet
+
+    jb .row
+    
+    pop bp
+    ret
+
+; draw_tile(tile_data *tile, uint16_t tile_x, uint16_t tile_y)
+; draw the given tile at the provided x,y tile coordinate
+draw_tile:
+
+    push bp
+    mov bp,sp
+
+    push es
+    push ax
+    push bx
+    push cx
+    push dx
+
+    push VMEM_SEGMENT
+    pop es
+
+    mov si,word [bp+4]          ; get the pointer to the tile data into si
+    mov cx,TILE_PIXEL_HEIGHT    ; number of pixels y per tile in CX
+    mov dx,word [bp+8]          ; we need to multiply the tile coordinate by the number of y pixels per tile          
+
+.row:
+
+    push cx
+    push dx
+
+    mov ax,PIXELS_PER_ROW
+    mul dx
+    xchg dx,ax                  ; get the absolute y start into cx
+
+    ; get x start into bx and adjust for absolute y
+    mov bx,word [bp+6]
+    add bx,dx
+
+    mov cx,TILE_PIXEL_WIDTH
+
+.column:
+
+    mov al,byte [ds:si]         ; get the next color byte
+    mov byte [es:bx],al         ; store it into the destination location
+    inc bx
+    inc si
+
+    loop .column                ; draw TILE_PIXEL_WIDTH bytes
+
+    pop dx
+    pop cx
+
+    inc dx
+    loop .row
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    pop es
+    pop bp
+    ret
