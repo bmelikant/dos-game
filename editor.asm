d552c241d4e25fb9c771c6e6a824745a08859cfd
Ben Melikant <bmmelikant@gmail.com>
2021-03-16 16:11:16 -0400

    [master] added tile editor pane and text display routines

--- editor.asm
+++ editor.asm
@@ -1,8 +1,13 @@
 org 0x0100
 bits 16
 
-%define BACKGROUND_COLOR 0x04
-%define SQUARE_COLOR 0x00
+%define EDITOR_BACKGROUND       50h
+%define BACKGROUND_COLOR        04h
+%define FONT_COLOR              77h
+%define EDITOR_SELECTED_COLOR   0fh
+    
+    jmp _main
+
 
 grass_block:
 
@@ -17,6 +22,10 @@ db 06h,06h,06h,06h,06h,06h,06h,06h,18h,06h
 db 06h,06h,14h,06h,14h,06h,06h,06h,06h,06h
 db 06h,06h,06h,06h,06h,06h,06h,06h,06h,06h
 
+; data area
+editor_current_pixel_x dw 0
+editor_current_pixel_y dw 0
+
 _main:
 
     push cs
@@ -28,27 +37,55 @@ _main:
     push word BACKGROUND_COLOR
     call fill_screen
     add sp,2
-
-    push word SQUARE_COLOR
-    push word 10
-    push word 20
-    push word 20
-    call do_square
-    add sp,8
     
-    push word 10
-    push word 50
-    push word 50
-    push word grass_block
-    call draw_tilemap
-    add sp,8
-
-    call await_keypress
+    call draw_tile_editor       ; draw the current state of the tile editor
+    call await_keypress         ; get user input
 
     call leave_vga_mode
 
     mov ax,0x4C00
     int 0x21
+
+; _cdecl draw_tile_editor()
+; draw the current tile editor to the screen
+draw_tile_editor:
+
+    push bp
+    mov bp,sp
+
+    ; draw a test character
+    xor dx,dx
+    mov dl,FONT_COLOR
+    push dx
+    push word editor_title
+    push 1
+    push 10
+    call put_string
+    add sp,8
+
+    ; draw tile editor border
+    push word EDITOR_BACKGROUND     ; rectangle color
+    push word 170                   ; 170 pixels high
+    push word 170                   ; 170 pixels wide
+    push word 20                    ; y coordinate
+    push word 10                    ; x coordinate
+    call do_frect
+    add sp,10
+
+    ; draw the current tile map
+    push word 16                    ; scale tile map up by 16
+    push word 25                    ; y coordinate
+    push word 15                    ; x coordinate
+    push word grass_block           ; tile to draw
+    call draw_tilemap
+    add sp,8
+
+    ; draw a frame around the selected pixel
+    
+
+    pop bp
+    ret
+
 
 await_keypress:
 
@@ -57,3 +94,6 @@ await_keypress:
     ret
 
 %include "vga-draw.inc"
+
+editor_title: db 'TILE EDITOR',0
+tile_buffer: times 100 db 0xff
