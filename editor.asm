5386aa618a0b6c72e90d1f471fa69f3c79ff0da2
Ben Melikant <bmmelikant@gmail.com>
2021-03-18 16:17:27 -0400

    [master] added palette drawing code

--- editor.asm
+++ editor.asm
@@ -5,26 +5,56 @@ bits 16
 %define BACKGROUND_COLOR        04h
 %define FONT_COLOR              77h
 %define EDITOR_SELECTED_COLOR   0fh
-    
+
+%define TILEMAP_SCALE 16
+
+%define EDITOR_POSITION_X   5
+%define EDITOR_POSITION_Y   10
+%define EDITOR_WIDTH        136
+%define EDITOR_HEIGHT       136
+
+%define TILE_SCALE 16
+%define TILE_EDITOR_X 9
+%define TILE_EDITOR_Y 14
+%define TILE_EDITOR_BOX_WIDTH 14
+%define TILE_EDITOR_BOX_HEIGHT TILE_EDITOR_BOX_WIDTH
+%define TILE_EDITOR_BOX_THICCNESS 1
+%define TILE_EDITOR_BOX_COLOR 0fh
+%define TEDIT_MAX_PIXEL_X 7
+%define TEDIT_MAX_PIXEL_Y 7
+
+%define PALETTE_POS_X 5
+%define PALETTE_POS_Y 140
+%define PALETTE_SQUARE_SZ 6
+%define PALETTE_ROWS 8
+%define PALETTE_COLUMNS 32
+
     jmp _main
 
 
 grass_block:
 
-db 02h,02h,02h,02h,02h,02h,02h,02h,02h,02h
-db 02h,06h,02h,06h,06h,06h,02h,06h,02h,02h
-db 02h,06h,06h,06h,06h,06h,06h,06h,02h,06h
-db 06h,06h,06h,06h,14h,06h,14h,06h,14h,06h
-db 14h,06h,14h,06h,06h,06h,06h,06h,18h,06h
-db 06h,06h,06h,06h,18h,06h,18h,06h,06h,06h
-db 06h,14h,06h,18h,06h,14h,06h,06h,06h,18h
-db 06h,06h,06h,06h,06h,06h,06h,06h,18h,06h
-db 06h,06h,14h,06h,14h,06h,06h,06h,06h,06h
-db 06h,06h,06h,06h,06h,06h,06h,06h,06h,06h
+db 02h,02h,02h,02h,02h,02h,02h,02h
+db 02h,06h,02h,06h,06h,06h,02h,06h
+db 02h,06h,06h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,14h,06h,14h,06h
+db 14h,06h,14h,06h,06h,06h,06h,06h
+db 06h,06h,06h,06h,18h,06h,18h,06h
+db 06h,14h,06h,18h,06h,14h,06h,06h
+db 06h,06h,06h,06h,06h,06h,06h,06h
 
 ; data area
-editor_current_x dw 0
-editor_current_y dw 0
+editor_current_x    db 0
+editor_current_y    db 0
+palette_current_x   db 0
+palette_current_y   db 0
+
+change_palette      db 0
+
+editor_x_max db TEDIT_MAX_PIXEL_X
+editor_y_max db TEDIT_MAX_PIXEL_Y
+palette_x_max db PALETTE_COLUMNS - 1
+palette_y_max db PALETTE_ROWS - 1
 
 _main:
 
@@ -41,7 +71,8 @@ _main:
     call fill_screen
     add sp,2
 
-    call draw_tile_editor       ; draw the current state of the tile editor
+    ;call draw_tile_editor       ; draw the current state of the tile editor
+    call draw_palette           ; and the color selector
     call draw
     call await_keypress         ; get user input
 
@@ -73,40 +104,40 @@ draw_tile_editor:
 
     ; draw tile editor border
     push word EDITOR_BACKGROUND     ; rectangle color
-    push word 170                   ; 170 pixels high
-    push word 170                   ; 170 pixels wide
-    push word 20                    ; y coordinate
-    push word 10                    ; x coordinate
+    push word EDITOR_HEIGHT         ; height
+    push word EDITOR_WIDTH          ; width
+    push word EDITOR_POSITION_Y     ; y coordinate
+    push word EDITOR_POSITION_X     ; x coordinate
     call do_frect
     add sp,10
 
 
     ; draw the current tile map
-    push word 16                    ; scale tile map up by 16
-    push word 25                    ; y coordinate
-    push word 15                    ; x coordinate
+    push word TILE_SCALE                    ; scale tile map up by 16
+    push word TILE_EDITOR_Y                 ; y coordinate
+    push word TILE_EDITOR_X                 ; x coordinate
     push word grass_block           ; tile to draw
     call draw_tilemap
     add sp,8
 
     ; compute the starting and ending points for the bounds box
-    mov dx,word [editor_current_x]      ; add the x offset
-    mov ax,16                           ; scaling factor
+    mov dx,word [editor_current_x]      ; current selected editor tile
+    mov ax,TILE_SCALE                   ; scaling factor
     mul dx
-    add ax,15
+    add ax,TILE_EDITOR_X                ; add the x offset of the tile map
     push ax                             ; save the x location onto the stack
 
     mov dx,word [editor_current_y]      ;
-    mov ax,16
+    mov ax,TILE_SCALE
     mul dx                      ; compute y position
-    add ax,25
+    add ax,TILE_EDITOR_Y
 
     pop dx                      ; restore the x value back into dx
 
-    push word 0fh               ; color
-    push word 1                 ; thiccness
-    push word 14                ; height
-    push word 14                ; width
+    push word TILE_EDITOR_BOX_COLOR
+    push word TILE_EDITOR_BOX_THICCNESS                     
+    push word TILE_EDITOR_BOX_HEIGHT
+    push word TILE_EDITOR_BOX_WIDTH
     push word ax                ; y loc is in ax
     push word dx                ; x loc in dx
     call draw_box
@@ -115,16 +146,86 @@ draw_tile_editor:
     pop bp
     ret
 
+draw_palette:
+
+    push bp
+    mov bp,sp
+
+    xor bx,bx
+    mov ax,PALETTE_POS_Y
+    mov dx,PALETTE_POS_X
+    mov cx,PALETTE_ROWS
+
+.row:
+
+    push cx
+    mov cx,PALETTE_COLUMNS
+
+.enter_loop:
+
+    push dx
+    push ax
+    push cx
+
+    push bx                         ; current color is in cx
+    push word PALETTE_SQUARE_SZ     ; y size in pixels
+    push word PALETTE_SQUARE_SZ     ; x size in pixels
+    push ax                         ; y pos is in ax
+    push dx                         ; x pos is in dx
+    call do_frect
+    add sp,10
+
+    pop cx
+    pop ax
+    pop dx
+
+    inc bx
+    add dx,PALETTE_SQUARE_SZ
+    loop .enter_loop
+
+    pop cx
+
+    mov dx,PALETTE_POS_X
+    add ax,PALETTE_SQUARE_SZ
+
+    loop .row
+
+    pop bp
+    ret
+
 await_keypress:
+
+    push di
+
+    ; set up whether we are changing the palette value or the tile pixel
+    mov al,byte [change_palette]
+    or al,al
+    jz .change_tile_pos
+
+    mov bx,palette_current_x
+    mov di,palette_current_y
+    jmp .key
+
+.change_tile_pos:
+
+    mov bx,editor_current_x
+    mov di,editor_current_y
+
+.key:
 
     mov ah,0x07
     int 0x21
+
+    cmp al,'t'
+    je .toggle
 
     cmp al,'s'
     je .column_down
 
     cmp al,'w'
     je .column_up
+    mov cx,PALETTE_ROWS
+    push cx
 
     cmp al,'a'
     je .column_left
@@ -139,10 +240,24 @@ await_keypress:
 
     jmp .done
 
+.toggle:
+
+    mov al,byte [change_palette]
+    or al,al
+    jz .toggle_on
+
+    mov byte [change_palette],0
+    jmp .done
+
+.toggle_on:
+
+    mov byte [change_palette],1
+    jmp .done
+
 .column_down:
 
     ; increase the y location of the draw box, up to a max of 10
-    cmp word [editor_current_y],9
+    cmp word [editor_current_y],TEDIT_MAX_PIXEL_Y
     jb .increment_editor_y
     xor dx,dx
     mov word [editor_current_y],dx
@@ -157,7 +272,7 @@ await_keypress:
 
     cmp word [editor_current_y],0
     jg .decrement_editor_y
-    mov word [editor_current_y],9
+    mov word [editor_current_y],TEDIT_MAX_PIXEL_Y
     jmp .done
 
 .decrement_editor_y:
@@ -169,7 +284,7 @@ await_keypress:
 
     cmp word [editor_current_x],0
     jg .decrement_editor_x
-    mov word [editor_current_x],9
+    mov word [editor_current_x],TEDIT_MAX_PIXEL_X
     jmp .done
 
 .decrement_editor_x:
@@ -179,7 +294,7 @@ await_keypress:
 
 .column_right:
 
-    cmp word [editor_current_x],9
+    cmp word [editor_current_x],TEDIT_MAX_PIXEL_X
     jb .increment_editor_x
     mov word [editor_current_x],0
     jmp .done
@@ -199,6 +314,7 @@ await_keypress:
 
 .exitfn:
 
+    pop di
     ret
 
 %include "vga-draw.inc"
